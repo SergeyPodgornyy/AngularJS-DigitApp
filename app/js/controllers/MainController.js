64502e3db3c0b4388084244066a813fb725560db
SergeyPodgornyy <sergey.podgornyy@yahoo.de>
2015-10-01 21:02:29 +0300

    Cleaning code - putting services to separate file

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,22 +1,7 @@
-digitApp.controller('MainCtrl', function ($scope, $http) {
-  $http.get('phones/phones.json').success(function(data, status, headers, config){
-    console.log('This is data: ',data,'\n\nThis is status: ', status,'\n\nThis is headers: ',headers,'\n\nThis is config: ',config);
+digitApp.controller('MainCtrl', ['$scope', 'phones', function ($scope, phones) {
+  phones.success(function(data){
     $scope.phones = data;
-  }).error(function(err){
-    return err;
   });
-
-  // $http.get(url, [config]);
-  // $http.post(url, data, [config]);
-  // $http.put(url, data, [config]);
-  // $http.patch(url, data, [config]);
-  // $http.delete(url, data, [config]);
-  // $http.head(url, data, [config]);
-  // $http.jsonp(url, data, [config]);
-
-  //url - url adress where we send request
-  //data - data for sending in request body
-  //config - JS Object which consist additional set up parameters
 
   $scope.title = 'Phones';
   $scope.sortField = undefined;
@@ -35,4 +20,4 @@ digitApp.controller('MainCtrl', function ($scope, $http) {
   $scope.isSortDown = function(fieldName){
   	return $scope.sortField === fieldName && $scope.reverse;
   }
-});
+}]);
---
+++ app/js/services/phones.js
@@ -0,0 +1,8 @@
+digitApp.factory('phones', ['$http', function($http){
+	return $http.get('phones/phones.json').success(function(data){
+		    return data;
+		  }).error(function(err){
+		    return err;
+  });
+}]);
+
