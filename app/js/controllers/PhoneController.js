be1d493c472902f83d2003d3bacd9168c51c1abc
SergeyPodgornyy <sergey.podgornyy@yahoo.de>
2015-10-02 02:24:52 +0300

    Adding detail information about products

--- app/js/controllers/PhoneController.js
+++ app/js/controllers/PhoneController.js
@@ -1,3 +1,14 @@
-digitApp.controller('PhoneCtrl', ['$scope', '$location', 'phones', '$routeParams', function ($scope, phones, $location, $routeParams) {
+digitApp.controller('PhoneCtrl', ['$scope', '$http', '$location', 'phones', '$routeParams', function ($scope, $http, phones, $location, $routeParams) {
 	$scope.phoneId = $routeParams.phoneId;
+
+	var url = 'phones/'+$routeParams.phoneId+'.json';
+
+	  $http.get(url).success(function(data) {
+	    $scope.phone = data;
+	    $scope.mainImageUrl = data.images[0];
+	  });
+
+	  $scope.setImage = function(imageUrl) {
+	    $scope.mainImageUrl = imageUrl;
+	  }
 }]);
