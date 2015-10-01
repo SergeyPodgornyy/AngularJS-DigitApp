c55de812fc9021461bfa7cdb8b35988419b6347d
SergeyPodgornyy <sergey.podgornyy@yahoo.de>
2015-10-01 22:11:26 +0300

    Filtering products

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -2,22 +2,4 @@ digitApp.controller('MainCtrl', ['$scope', 'phones', function ($scope, phones) {
   phones.success(function(data){
     $scope.phones = data;
   });
-
-  $scope.title = 'Phones';
-  $scope.sortField = undefined;
-  $scope.reverse = false;
-  $scope.sort = function(fieldName){
-  	if($scope.sortField === fieldName){
-  		$scope.reverse = !$scope.reverse;
-  	} else {
-  		$scope.sortField = fieldName;
-  		$scope.reverse = false;
-  	}
-  };
-  $scope.isSortUp = function(fieldName){
-  	return $scope.sortField === fieldName && !$scope.reverse;
-  }
-  $scope.isSortDown = function(fieldName){
-  	return $scope.sortField === fieldName && $scope.reverse;
-  }
 }]);
