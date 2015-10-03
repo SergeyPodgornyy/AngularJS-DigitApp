d6f538380b9881f2269a4b3eedbe6d256c319b01
SergeyPodgornyy <sergey.podgornyy@yahoo.de>
2015-10-03 21:37:26 +0300

    Adding order form

--- app/js/controllers/PhoneController.js
+++ app/js/controllers/PhoneController.js
@@ -1,4 +1,4 @@
-digitApp.controller('PhoneCtrl', ['$scope', '$http', '$location', 'phones', '$routeParams', function ($scope, $http, phones, $location, $routeParams) {
+digitApp.controller('PhoneCtrl', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
 	$scope.phoneId = $routeParams.phoneId;
 
 	var url = 'phones/'+$routeParams.phoneId+'.json';
@@ -6,9 +6,34 @@ digitApp.controller('PhoneCtrl', ['$scope', '$http', '$location', 'phones', '$ro
 	  $http.get(url).success(function(data) {
 	    $scope.phone = data;
 	    $scope.mainImageUrl = data.images[0];
+	    $scope.prices = data.prices;
 	  });
 
-	  $scope.setImage = function(imageUrl) {
+	$scope.toggleActive = function(s){
+		s.active = !s.active;
+	};
+
+	// Helper method for calculating the total price
+
+	$scope.total = function(){
+
+		var total = 0;
+
+		// Use $scope.prices[0].price in future
+
+		// Use the angular forEach helper method to
+		// loop through the services array:
+
+		angular.forEach($scope.prices, function(s){
+			if (s.active){
+				total+= s.price;
+			}
+		});
+
+		return total;
+	};
+
+	$scope.setImage = function(imageUrl) {
 	    $scope.mainImageUrl = imageUrl;
-	  }
+	}
 }]);
