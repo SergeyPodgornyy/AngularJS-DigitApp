8e0884f9d698a5e5a78086afebce5f7c4db0b96f
SergeyPodgornyy <sergey.podgornyy@yahoo.de>
2015-10-04 04:30:40 +0300

    Adding Feed button

---
+++ app/js/directives/plusOne.js
@@ -0,0 +1,12 @@
+digitApp.directive('plusOne', function(){
+	return { 
+    restrict: 'E', 
+    scope: { }, 
+    templateUrl: 'js/directives/plusOne.html',
+    link: function(scope, element, attrs) { 
+      scope.like = function() {
+        element.toggleClass('btn-like');
+      } 
+    }
+  };
+});
